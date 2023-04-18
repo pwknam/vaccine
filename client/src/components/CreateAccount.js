1ebf4cf7a931c35930e6c565c0f22899e1a5efb0
Kyushik <kyushik95@gmail.com>
2023-04-18 18:48:00 -0400

    good work

--- client/src/components/CreateAccount.js
+++ client/src/components/CreateAccount.js
@@ -1,43 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
 import "../App.css";
 
 import { useNavigate } from "react-router-dom";
 
-function CreateAccount() {
-  const navigate = useNavigate();
+function CreateAccount({ setNewUser }) {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [option, setOption] = useState("Choose an option");
 
-  function handleInstitutionDashboardPage() {
-    navigate({ pathname: "/institutionDashboard" });
+  const navigate = useNavigate();
+  console.log(setNewUser);
+  // function handleInstitutionDashboardPage() {
+  //   navigate({ pathname: "/institutionDashboard" });
+  // }
+
+  function handleCreateAccountSubmit(e) {
+    e.preventDefault();
+    fetch(
+      `http://127.0.0.1:5555/signup_${
+        option === "Issuer" ? "issuer" : "validator"
+      }`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: name,
+          password: password,
+        }),
+      }
+    )
+      .then((r) => r.json())
+      .then((data) => {
+        console.log(setNewUser, data);
+        setNewUser(data);
+        navigate({
+          pathname: `${
+            option === "Issuer" ? "/institutionDashboard" : "/searchPage"
+          }`,
+        });
+      });
   }
 
   return (
     <div className="setupInstitution">
       <h1>Setup Institution</h1>
-      <form className="createAccountForm">
+      <form className="createAccountForm" onSubmit={handleCreateAccountSubmit}>
         <div className="formDiv">
           <label className="formLabel">Name of Institution</label>
-          <input type="text" className="formInput"></input>
+          <input
+            type="text"
+            className="formInput"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          ></input>
         </div>
         <div className="formDiv">
           <label className="formLabel">Type of Institution</label>
-          <select className="formInput">
-            <option></option>
-            <option>Medical Institution</option>
-            <option>Event Host</option>
-            <option>Transporation</option>
+          <select
+            className="formInput"
+            onChange={(e) => setOption(e.target.value)}
+            value={option}
+          >
+            <option>Choose an option</option>
+            <option>Issuer</option>
+            <option>Validator</option>
           </select>
         </div>
         <div className="formDiv">
           <label className="formLabel">Set Password</label>
-          <input type="text" className="formInput"></input>
+          <input
+            type="password"
+            className="formInput"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          ></input>
         </div>
 
-        <button
-          className="createAccountButton"
-          onClick={handleInstitutionDashboardPage}
-        >
-          Create Account
-        </button>
+        <button className="createAccountButton">Create Account</button>
       </form>
     </div>
   );
--- client/src/components/InstitutionDashboard.js
+++ client/src/components/InstitutionDashboard.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../App.css";
 import PatientCard from "./PatientCard";
 import { useNavigate } from "react-router-dom";
 
-function InstitutionDashboard() {
+function InstitutionDashboard({ userInfo }) {
   const navigate = useNavigate();
 
   function handleNewPatientForm() {
@@ -13,7 +13,7 @@ function InstitutionDashboard() {
   return (
     <div className="institutionDashboardPage">
       <div className="topBar">
-        <h1>NYU Langone Hospital</h1>
+        <h1>{userInfo.username}</h1>
         <button onClick={handleNewPatientForm}>New Patient</button>
       </div>
       <div className="searchBarDiv">
