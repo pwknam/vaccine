40d5455e51a4fe1813d9394438342cfcc2abe415
Michelle <choi.michelle.12@gmail.com>
2023-04-18 22:54:31 -0400

    ocr built into search page

--- client/src/components/SearchPage.js
+++ client/src/components/SearchPage.js
@@ -1,5 +1,6 @@
 import "../App.css";
 import { useNavigate } from "react-router-dom";
+import Home from "./Home/Home";
 
 function SearchPage() {
   const navigate = useNavigate();
@@ -11,13 +12,15 @@ function SearchPage() {
 
   return (
     <div className="searchContainer">
-      <h1>Search Patient</h1>
+      <h1>Search Patient or Upload License Photo</h1>
       <form className="searchForm" onSubmit={handleSubmit}>
         <input
           type="text"
           className="searchBar"
           placeholder="Drivers License Number"
         ></input>
+        {/* IF YOU WANT TO GET RID OF THE UGLY OCR THEN COMMENT OUT THE HOME COMPONENT - I NEED TO RENAME ANYWAYS */}
+        <Home/>
         <button className="createAccountButton">Search</button>
       </form>
     </div>
