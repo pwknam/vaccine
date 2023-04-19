91c52c59ae4923d64a803b82cf0bd5ea5c4ef2d0
Michelle <choi.michelle.12@gmail.com>
2023-04-18 21:22:13 -0400

    api plugged in, need to return license

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -7,26 +7,39 @@ const Home = () => {
     const [image, setImage] = useState('');
 
     const handleSubmit= (e) => {
-        alert("Form submitted");
-        fetch('/http://127.0.0.1:5555/upload', {
+        // alert("Form submitted");
+        e.preventDefault()
+        fetch('http://127.0.0.1:5555/upload', {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json'
             },
             body: JSON.stringify({ image })
           })
-          .then(response => {
-            if (response.ok) {
-              console.log(response.data);
-            } else {
-              console.log('Failed to upload data.');
+          .then(res => {
+            if (!res.ok) {
+              throw new Error('Network response was not ok');
             }
+            return res.json();
+          })
+          .then(data => {        
+            console.log(data);
           })
           .catch(error => {
             console.log(error);
           });
-        };
+            // if (response.ok) {
+            //   console.log(response);
+            // } else {
+            //   console.log('Failed to upload data.');
+            // }
+          
+        //   .catch(error => {
+        //     console.log(error);
+        //   });
+        
       
+    }
         // setName('');
         // setEmail('');
         // setImage()
@@ -34,21 +47,21 @@ const Home = () => {
     //     event.preventDefault();
     //     console.log('hi')
 
-    //     // Specify variables for use in script below
-    //     // const apiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJnb29nbGUtb2F1dGgyfDEwNjkxNDc5MTMzMjAzMDMzOTY2OSIsImVtYWlsIjoibWNob2k0MTk0QGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJpYXQiOjE2ODE2NjkwNzU5OTZ9.zYaCZMVgMnUWurL7CGa0JGrR29CIolKRlSvgqDZjTTU'
-    //     // const queueId = '93c11f04-c119-4f78-a7b5-036b617fda49'
+        // Specify variables for use in script below
+        // const apiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJnb29nbGUtb2F1dGgyfDEwNjkxNDc5MTMzMjAzMDMzOTY2OSIsImVtYWlsIjoibWNob2k0MTk0QGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJpYXQiOjE2ODE2NjkwNzU5OTZ9.zYaCZMVgMnUWurL7CGa0JGrR29CIolKRlSvgqDZjTTU'
+        // const queueId = '93c11f04-c119-4f78-a7b5-036b617fda49'
 
-    //     // // Specify the path to the file you would like to process
-    //     // const fileLocation = '...../uploads/ocr';
+        // // Specify the path to the file you would like to process
+        // const fileLocation = '...../uploads/ocr';
 
-    //     // // Create client
-    //     // const client = new Butler(apiKey)
+        // // Create client
+        // const client = new Butler(apiKey)
 
-    //     // const file = fs.createReadStream(fileLocation);
-    //     // client.extractDocument(queueId, file).then((x) => {
-    //     // console.log(x);
-    //     // });
-    //     // send the image to the server using fetch or axios
+        // const file = fs.createReadStream(fileLocation);
+        // client.extractDocument(queueId, file).then((x) => {
+        // console.log(x);
+        // });
+        // send the image to the server using fetch or axios
     // };
 
 
--- server/app.py
+++ server/app.py
@@ -3,6 +3,8 @@ from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 from models import db
+from butler import Client
+
 
 
 app = Flask(__name__)
@@ -18,6 +20,53 @@ api = Api(app)
 class Upload(Resource):
     def post(self):
         data = request.get_json()
+        # Write image data to file
+        with open('./storage/ocr_image.jpeg', 'wb') as f:
+            f.write(request.data)
+        
+        # Make sure to first install the SDK using 'pip install butler-sdk'
+ 
+
+        # Specify variables for use in script below
+        api_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJnb29nbGUtb2F1dGgyfDEwNjkxNDc5MTMzMjAzMDMzOTY2OSIsImVtYWlsIjoibWNob2k0MTk0QGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJpYXQiOjE2ODE2NjkwNzU5OTZ9.zYaCZMVgMnUWurL7CGa0JGrR29CIolKRlSvgqDZjTTU'
+        queue_id = '93c11f04-c119-4f78-a7b5-036b617fda49'
+
+        # Specify the path to the file you would like to process
+        file_location = './storage/ocr_image.jpeg'
+
+        fileinfo = Client(api_key).extract_document(queue_id, file_location)
+        formFields = fileinfo.to_dict()['formFields']
+        for field in formFields:
+            if field['fieldName'] == "Document Number":
+                license = field['value']
+        # license ? 
+        response = license if license else "No License found"
+        return make_response(response, 200)
+
+        # const capturedImage = async (req, res, next) => {
+        # try {
+        # path = './storage/ocr_image.jpeg'   
+        # let imgdata = req.body.img;                 // get img as base64
+        # const base64Data = imgdata.replace(/^data:([A-Za-z-+/]+);base64,/, '');     // convert base64
+        # fs.writeFileSync(path, image,  {encoding: 'base64'});                  
+        # Tesseract.recognize(
+        #     'http://localhost:5001/img/ocr_image.jpeg',
+        #     'eng',
+        #     { logger: m => console.log(m) }
+        # )
+        # .then(({ data: { text } }) => {
+        #     console.log(text)
+        #     return res.send({
+        #         image: imgdata,
+        #         path: path,
+        #         text: text
+        #     });
+        # })
+
+        # } catch (e) {
+        #     next(e);
+        # }
+    
 
         # new_plant = Plant(
         #     name=data['name'],
