c829187b9bbbb32342c0189dd953b4ff39476aa1
ianstrom <iankstrom@gmail.com>
2023-04-21 10:01:08 -0400

    asdf

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -26,7 +26,7 @@ function App() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    fetch('https://vaccine-pt72.onrender.com/check_session')
+    fetch('/check_session')
     .then(r => {
       if (r.ok) {
         r.json().then(data => {
