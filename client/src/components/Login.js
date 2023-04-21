fceb645767bc12dab4dfd5c968097bebb5b73f3a
ianstrom <iankstrom@gmail.com>
2023-04-20 20:19:54 -0400

    testing

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -10,6 +10,9 @@ function Login({ setNewUser }) {
   const navigate = useNavigate();
 
   function handleLogin(e) {
+    fetch(`/patients`)
+    .then(r => r.json())
+    .then(data => console.log(data))
     e.preventDefault();
     fetch(`/login`, {
       method: "POST",
