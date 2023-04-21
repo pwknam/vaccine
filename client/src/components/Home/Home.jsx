99fecf0f9b79cdc85e364a7318fa5275b09ba2b3
Michelle <choi.michelle.12@gmail.com>
2023-04-21 11:35:12 -0400

    basic loading works

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,18 +1,18 @@
 import React, { useState } from 'react'
 import { WebcamCapture} from '../Webcam/Webcam'
 import { useNavigate } from "react-router-dom";
-import "./homeStyles.css"
+import './homeStyles.css'
 
 
-const Home = ({setSearch}) => {
+const Home = ({ setSearch }) => {
     const [image, setImage] = useState('');
-    const[driverslicense, setDriverslicense] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [driverslicense, setDriverslicense] = useState('');
     const navigate = useNavigate();
 
-
-    const handleSubmit= (e) => {
-        // alert("Form submitted");
+    const handleSubmit = (e) => {
         e.preventDefault()
+        setLoading(true);
         fetch('/upload', {
             method: 'POST',
             headers: {
@@ -21,33 +21,41 @@ const Home = ({setSearch}) => {
             body: JSON.stringify({ image })
           })
           .then(res => {
+            setLoading(false);
             if (!res.ok) {
               throw new Error('Network response was not ok');
             }
             return res.json();
           })
           .then(data => {        
-            console.log(data);
             setSearch(data.license)
             navigate({ pathname: "/validatorDashboard" });
           })
           .catch(error => {
+            setLoading(false);
             console.log(error);
           });
-          // navigate({ pathname: "/validatorDashboard" });
     }
 
-
-
     return (
-       
-                    <form className="form">
-                        <WebcamCapture image={image} handleSubmit={handleSubmit} setImage = {setImage} />
-                        <button type="submit" className='button-47' onClick={(e) => handleSubmit(e)}>Submit</button>
-                        {driverslicense && <h1>{driverslicense}</h1>}    
-
-                    </form>
-
+        <form className="form">
+            
+            {loading ? (
+              <div class="la-ball-atom">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+            
+            ) : (<>
+              <WebcamCapture image={image} setImage={setImage} />
+              <button className="button-47" type="submit" id="login-button" onClick={(e) => handleSubmit(e)}>Submit</button>
+              </>
+            )}
+            {driverslicense && <h1>{driverslicense}</h1>}
+        </form>
     )
 }
-export default Home
+
+export default Home;
